b05cadef8a422d6231e68b64e8640c8f63a45c05
Arseny Garelyshev <monstrag@gmail.com>
2025-02-03 18:56:11 +0100

    refactor: screw the E-tag, lets just rely on "last-modified"
    That actually is enough for browser to cache regular items, but now
    we don't need to re-calc each file on each request which was slow.

--- pages/music/getSong.go
+++ pages/music/getSong.go
@@ -1,13 +1,13 @@
 package music
 
 import (
-	"go-server/pages"
 	"go-server/setup/reqRes"
+	"net/http"
 	"path/filepath"
 )
 
 func GetSongHandler(w reqRes.MyWriter, r *reqRes.MyRequest) {
 	pathQueryParam := r.PathValue("path")
 	filename := filepath.Join(r.AppConfig.SongsFolder, pathQueryParam)
-	pages.ServeFile(w, r, filename)
+	http.ServeFile(w, &r.Request, filename)
 }
--- pages/public.go
+++ pages/public.go
@@ -2,11 +2,12 @@ package pages
 
 import (
 	"go-server/setup/reqRes"
+	"net/http"
 	"path/filepath"
 )
 
 func PublicHandler(w reqRes.MyWriter, r *reqRes.MyRequest) {
 	pathQueryParam := r.PathValue("path")
 	filename := filepath.Join("public", pathQueryParam)
-	ServeFile(w, r, filename)
+	http.ServeFile(w, &r.Request, filename)
 }
