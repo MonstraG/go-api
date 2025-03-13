4eb17d52e54f1e3ec184fd8b3469f7ed390821de
Arseny Garelyshev <monstrag@gmail.com>
2025-03-13 21:17:47 +0100

    feat: multiple rooms!

--- setup/websockets/websockets.go
+++ setup/websockets/websockets.go
@@ -1,6 +1,7 @@
 package websockets
 
 import (
+	"errors"
 	"go-server/setup"
 	"go-server/setup/reqRes"
 	"log"
@@ -12,19 +13,51 @@ import (
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		return true // Allow all connections (customize for security)
+		return true // maybe add origins, but then I have to deal with nginx
 	},
+}
+
+type Room struct {
+	Id    string
+	Peers map[string]*Peer
+	Mutex sync.Mutex
 }
 
 type Peer struct {
 	Id   string
+	Room string
 	Conn *websocket.Conn
 }
 
 var (
-	peers      = make(map[string]*Peer)
-	peersMutex sync.Mutex
+	rooms      = make(map[string]*Room)
+	roomsMutex sync.Mutex
 )
+
+func getOrCreateRoom(roomId string) *Room {
+	roomsMutex.Lock()
+	defer roomsMutex.Unlock()
+
+	room, exists := rooms[roomId]
+	if !exists {
+		room = &Room{
+			Id:    roomId,
+			Peers: make(map[string]*Peer),
+		}
+		rooms[roomId] = room
+	}
+	return room
+}
+
+func getRoom(roomId string) (*Room, error) {
+	roomsMutex.Lock()
+	defer roomsMutex.Unlock()
+	room, exists := rooms[roomId]
+	if !exists {
+		return nil, errors.New("Failed to find room " + roomId)
+	}
+	return room, nil
+}
 
 func parseMessage(connection *websocket.Conn) (map[string]any, error) {
 	var msg map[string]any
@@ -32,16 +65,23 @@ func parseMessage(connection *websocket.Conn) (map[string]any, error) {
 	return msg, err
 }
 
-func rememberPeer(peer *Peer) {
-	peersMutex.Lock()
-	defer peersMutex.Unlock()
-	peers[peer.Id] = peer
+func rememberPeer(peer *Peer, room *Room) {
+	room.Mutex.Lock()
+	defer room.Mutex.Unlock()
+	room.Peers[peer.Id] = peer
 }
 
-func forgetPeer(peer *Peer) {
-	peersMutex.Lock()
-	defer peersMutex.Unlock()
-	delete(peers, peer.Id)
+func forgetPeer(peer *Peer, room *Room) {
+	room.Mutex.Lock()
+	defer room.Mutex.Unlock()
+	delete(room.Peers, peer.Id)
+
+	// If the room is empty, remove it
+	if len(room.Peers) == 0 {
+		roomsMutex.Lock()
+		defer roomsMutex.Unlock()
+		delete(rooms, room.Id)
+	}
 }
 
 type MessageAssignId struct {
@@ -54,7 +94,7 @@ type MessageUserLeaves struct {
 }
 
 func HandleWebSocket(w reqRes.MyWriter, r *reqRes.MyRequest) {
-	conn, err := upgrader.Upgrade(w, &r.Request, nil)
+	connection, err := upgrader.Upgrade(w, &r.Request, nil)
 	if err != nil {
 		log.Printf("WebSocket upgrade error error: \n%v\n", err)
 		return
@@ -64,38 +104,58 @@ func HandleWebSocket(w reqRes.MyWriter, r *reqRes.MyRequest) {
 		if err != nil {
 			log.Printf("WebSocket connection close error: \n%v\n", err)
 		}
-	}(conn)
+	}(connection)
 
-	peer := &Peer{Id: setup.RandId(), Conn: conn}
+	peerId := setup.RandId()
+	log.Printf("New peer %s trying to connect, wating for the room message\n", peerId)
+
+	initialMsg, err := parseMessage(connection)
+	if err != nil {
+		log.Printf("Failed to read initial message: \n%v\n", err)
+		return
+	}
+
+	roomId, ok := initialMsg["roomId"].(string)
+	if !ok || roomId == "" {
+		log.Println("Client did not send a valid roomId, closing connection.")
+		err := connection.Close()
+		if err != nil {
+			log.Printf("WebSocket connection close error: \n%v\n", err)
+		}
+		return
+	}
+
+	room := getOrCreateRoom(roomId)
+	peer := &Peer{Id: peerId, Room: room.Id, Conn: connection}
 
 	messageAssignId := MessageAssignId{
 		YourId: peer.Id,
 	}
 
-	ok := sendMessage(peer, messageAssignId)
+	ok = sendMessage(peer, messageAssignId)
 	if !ok {
-		log.Printf("Failed to send messageAssignId to peer %s, I guess we just drop the connection.\n", peer.Id)
+		log.Printf("Failed to send messageAssignId to peer %s in room %s, I guess we just drop the connection.\n", peer.Id, peer.Room)
 		return
 	}
 
-	log.Printf("New WebRTC peer connected: %s\n", peer.Id)
-	rememberPeer(peer)
+	log.Printf("New WebRTC peer connected: %s in room %s\n", peer.Id, peer.Room)
+	rememberPeer(peer, room)
 
 	for {
-		msg, err := parseMessage(conn)
+		msg, err := parseMessage(connection)
 		if err != nil {
 			log.Printf("Error reading JSON: \n%v\n", err)
 			break
 		}
 
-		handleMessage(peer.Id, msg)
+		handleMessage(peer, msg)
 	}
 
 	// tell everyone that they are going away
-	handleGlobalMessage(peer.Id, MessageUserLeaves{FromId: peer.Id, Bye: true})
+	handleRoomMessage(peer, MessageUserLeaves{FromId: peer.Id, Bye: true})
 
 	// Cleanup on disconnect
-	forgetPeer(peer)
+	forgetPeer(peer, room)
 	log.Printf("WebRTC peer disconnected: %s\n", peer.Id)
 }
 
@@ -108,43 +168,55 @@ func sendMessage(peer *Peer, message any) bool {
 	return true
 }
 
-func handleMessage(senderId string, message map[string]any) {
+func handleMessage(sender *Peer, message map[string]any) {
 	targetId, hasTarget := message["toId"].(string)
 	if hasTarget {
-		handleTargetedMessage(senderId, targetId, message)
+		handleTargetedMessage(sender, targetId, message)
 	} else {
-		handleGlobalMessage(senderId, message)
+		handleRoomMessage(sender, message)
 	}
 }
 
-func handleTargetedMessage(senderId string, targetId string, message map[string]any) {
-	peersMutex.Lock()
-	defer peersMutex.Unlock()
-
-	recipient, found := peers[targetId]
-	if !found {
-		log.Printf("Sender %s tried to send to target %s but they were not found!\n", senderId, targetId)
+func handleTargetedMessage(sender *Peer, targetId string, message map[string]any) {
+	room, err := getRoom(sender.Room)
+	if err != nil {
+		log.Printf("Sender %s tried to send to target %s but room %s was not found!\n", sender.Id, targetId, sender.Room)
 		return
 	}
 
-	err := recipient.Conn.WriteJSON(message)
+	room.Mutex.Lock()
+	defer room.Mutex.Unlock()
+
+	recipient, found := room.Peers[targetId]
+	if !found {
+		log.Printf("Sender %s tried to send to target %s but they were not found!\n", sender.Id, targetId)
+		return
+	}
+
+	err = recipient.Conn.WriteJSON(message)
 	if err != nil {
-		log.Printf("Sender %s tried to send to target %s but sending failed: \n%v\n", senderId, targetId, err)
+		log.Printf("Sender %s tried to send to target %s but sending failed: \n%v\n", sender.Id, targetId, err)
 	}
 }
 
-func handleGlobalMessage(senderId string, message any) {
-	peersMutex.Lock()
-	defer peersMutex.Unlock()
+func handleRoomMessage(sender *Peer, message any) {
+	room, err := getRoom(sender.Room)
+	if err != nil {
+		log.Printf("Sender %s tried to send to room %s, but it was not found!\n", sender.Id, sender.Room)
+		return
+	}
 
-	for _, peer := range peers {
-		if peer.Id == senderId {
+	room.Mutex.Lock()
+	defer room.Mutex.Unlock()
+
+	for _, peer := range room.Peers {
+		if peer.Id == sender.Id {
 			continue
 		}
 
-		err := peer.Conn.WriteJSON(message)
+		err = peer.Conn.WriteJSON(message)
 		if err != nil {
-			log.Printf("Sender %s tried to send to target %s but sending failed: \n%v\n", senderId, peer.Id, err)
+			log.Printf("Sender %s tried to send to target %s but sending failed: \n%v\n", sender.Id, peer.Id, err)
 		}
 	}
 }
