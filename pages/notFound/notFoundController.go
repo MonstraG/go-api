09d896a4a31f34a7b28c4d72845a181e49bbd15b
Arseny Garelyshev <monstrag@gmail.com>
2025-10-20 21:10:04 +0200

    fix: return 404 in not found controller

--- pages/notFound/notFoundController.go
+++ pages/notFound/notFoundController.go
@@ -4,6 +4,7 @@ import (
 	"go-api/infrastructure/reqRes"
 	"go-api/pages"
 	"html/template"
+	"net/http"
 )
 
 var notFoundTemplate = template.Must(template.ParseFiles("pages/base.gohtml", "pages/notFound/notFound.gohtml"))
@@ -11,5 +12,6 @@ var notFoundTemplate = template.Must(template.ParseFiles("pages/base.gohtml", "p
 func Show404(w reqRes.MyResponseWriter, r *reqRes.MyRequest) {
 	pageData := pages.NewPageData(r, "404: page not found")
 
+	w.WriteHeader(http.StatusNotFound)
 	w.RenderTemplate(notFoundTemplate, pageData)
 }
