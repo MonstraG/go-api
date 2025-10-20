8f21170a0919f8f8c91b66251a5180ce6e8287d1
Arseny Garelyshev <monstrag@gmail.com>
2025-10-20 21:03:45 +0200

    fix: defer closing properly

--- pages/fileExplorer/PutFile.go
+++ pages/fileExplorer/PutFile.go
@@ -1,11 +1,13 @@
 package fileExplorer
 
 import (
+	"errors"
 	"fmt"
 	"go-api/infrastructure/helpers"
 	"go-api/infrastructure/myLog"
 	"go-api/infrastructure/reqRes"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -15,6 +17,8 @@ const hundredMegs = 100 << 20
 
 func (controller *Controller) PutFile(w reqRes.MyResponseWriter, r *reqRes.MyRequest) {
 	pathQueryParam := r.PathValue("path")
+
+	folder := filepath.Join(controller.explorerRoot, pathQueryParam)
 
 	err := r.ParseMultipartForm(hundredMegs)
 	if err != nil {
@@ -29,40 +33,45 @@ func (controller *Controller) PutFile(w reqRes.MyResponseWriter, r *reqRes.MyReq
 		return
 	}
 
-	folder := filepath.Join(controller.explorerRoot, pathQueryParam)
-
 	for _, handler := range files {
-		formFile, err := handler.Open()
+		err := writeFile(folder, handler)
 		if err != nil {
-			message := fmt.Sprintf("Failed to retrieve file: \n%v", err)
-			w.Error(message, http.StatusInternalServerError)
-		}
-
-		fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-		fmt.Printf("File Size: %+v\n", handler.Size)
-		fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-		path := filepath.Join(folder, handler.Filename)
-
-		diskFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, filePermissions)
-		if err != nil {
-			message := fmt.Sprintf("Failed to retrieve file: \n%v", err)
-			myLog.Error.Logf(message)
-			renderExplorer(w, folder, pathQueryParam, message)
+			myLog.Error.Logf(err.Error())
+			renderExplorer(w, folder, pathQueryParam, err.Error())
 			return
 		}
-
-		_, err = io.Copy(diskFile, formFile)
-		if err != nil {
-			message := fmt.Sprintf("Failed to save file: \n%v", err)
-			myLog.Error.Logf(message)
-			renderExplorer(w, folder, pathQueryParam, message)
-			return
-		}
-
-		helpers.CloseSafely(formFile)
-		helpers.CloseSafely(diskFile)
 	}
 
 	renderExplorer(w, folder, pathQueryParam, "File(s) uploaded!")
+}
+
+func writeFile(folder string, handler *multipart.FileHeader) error {
+	formFile, err := handler.Open()
+	defer helpers.CloseSafely(formFile)
+	if err != nil {
+		message := fmt.Sprintf("Failed to open file handler: \n%v", err)
+		return errors.New(message)
+	}
+
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	path := filepath.Join(folder, handler.Filename)
+
+	diskFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, filePermissions)
+	defer helpers.CloseSafely(diskFile)
+	if err != nil {
+		message := fmt.Sprintf("Failed to open file on disk: \n%v", err)
+		return errors.New(message)
+	}
+
+	_, err = io.Copy(diskFile, formFile)
+	if err != nil {
+		message := fmt.Sprintf("Failed to copy file: \n%v", err)
+		return errors.New(message)
+
+	}
+
+	return nil
 }
